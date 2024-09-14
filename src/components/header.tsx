e151b09cf05b4dff9cf8149ab8a2baf6100d2e66
Jessica.Silva <jessica.silva@class-solutions.com.br>
2024-09-14 16:47:37 -0300

    estilizando header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { UseMyThemeContext } from "../contexts/theme-context"
 import { Link } from "react-router-dom";
+import '../main.css'
 
 export default function Header (){
     const {setThemeValue} = UseMyThemeContext();
     return(
-        <header>
+        <header className="flex flex-row items-center justify-between py-[15px] px-[30px] bg-gray-20">
             <h1>LOGO EMPRESA</h1>
-            <nav>
-                <ul>
+            <nav className="w-6/12">
+                <ul className="flex list-none flex flex-row items-center justify-between">
                     <li><Link to="/"> Home</Link></li>
                     <li> <Link to="/pages/sobre">Sobre</Link></li>
-                    <li><Link to="/">Suport</Link></li>
+                    <li><Link to="/pages/suport">Suport</Link></li>
                 </ul>
             </nav>
-            <button onClick={() => {setThemeValue("Light")}}>Light</button>
-            <button onClick={() => {setThemeValue("Dark")}}>Dark</button>
+            <button className="py-[10px] px-[15px] bg-red-300" onClick={() => {setThemeValue("Light")}}>Light</button>
+            <button className="py-[10px] px-[15px] bg-blue-300" onClick={() => {setThemeValue("Dark")}}>Dark</button>
         </header>
     )
 }
