8de58175e1fff55dc3e80414987760b36b49407d
Jessica.Silva <jessica.silva@class-solutions.com.br>
2024-09-14 10:16:06 -0300

    configurando rotas

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,31 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Sobre from './pages/sobre';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/pages/sobre",
+    element: <Sobre/>,
+  },
+  
+]);
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
