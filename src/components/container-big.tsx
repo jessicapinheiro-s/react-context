c8db832c028797c19ca70cce2adea8d3d32d3762
Jessica.Silva <jessica.silva@class-solutions.com.br>
2024-09-15 09:11:03 -0300

    Criando componente container para utilizar como base nas paginas.

---
+++ src/components/container-big.tsx
@@ -0,0 +1,14 @@
+interface ContainerBigProps {
+    Title: string;
+    Description: string;
+}
+
+export function ContainerBig (props: ContainerBigProps) {
+    const {Title, Description} = props;
+    return(
+        <div className="block py-[25px] px-[50px]">
+            <h1 className="text-base text-3xl text-center">{Title}</h1>
+            <p className="text-base text-left">{Description}</p>
+        </div>
+    )
+}
