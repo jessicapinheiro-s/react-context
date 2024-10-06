1d9db41542620ba15444e22ba7e8bdf1688897ba
Jessica.Silva <jessica.silva@class-solutions.com.br>
2024-10-06 08:44:23 -0300

    configurando paleta de cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "back-footer": "#001a4b", // Replace with your hex color
-        "back-button-padrao": "#001a4b", // Replace with your hex color
+        "back-footer": "#002a7a", // Replace with your hex color
+        "back-button-padrao": "#002a7a", // Replace with your hex color
+        "back-button-orange": "#ed0d28",
+        "back-dark":"#18181b", // Replace with your hex color
+        "back-light":"#fff", // Replace with your hex color
+        "color-dark":"#fff", // Replace with your hex color
+        "color-light":"#000", // Replace with your hex color
+        "border-dark":"#71717a", // Replace with your hex color
+        "components-dark":"#27272a", // Replace with your hex color
       },
     },
   },
