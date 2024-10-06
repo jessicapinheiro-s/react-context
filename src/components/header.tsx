8ae46ae44d79501bfe9a6f2ec60bbaba15bdf8eb
Jessica.Silva <jessica.silva@class-solutions.com.br>
2024-10-06 14:48:30 -0300

    atribuindo responsividade ao menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,25 +2,42 @@ import React, { useState } from "react";
 import { UseMyThemeContext } from "../contexts/Theme-context"
 import { Link } from "react-router-dom";
 import '../main.css'
-import { Moon, SunMoon } from "lucide-react";
+import { CircleX, Menu, Moon, SunMoon } from "lucide-react";
 import { motion } from "framer-motion";
 import ThemeIcon from "./Theme-Icon";
 
 export default function Header() {
+    const styleLiMobile = 'py-[10px] px-[40px] hover:bg-back-button-orange sm:p-0 sm:hover:bg-transparent';
+    const styleLiLink = 'w-full block';
+    const [classNav, setClassNav] = useState('sm:w-4/12 absolute top-0 bottom-0 right-0 w-8/12 sm:relative sm:bg-transparent bg-components-dark z-10 hidden');
+    
+    function openMenu(event: React.MouseEvent<SVGSVGElement>): void {
+        event.stopPropagation();
+        setClassNav('sm:w-4/12 absolute top-0 bottom-0 right-0 w-8/12 sm:relative sm:bg-transparent bg-components-dark z-10')
+    }
+
+    function closeMenu(event: React.MouseEvent<SVGSVGElement>): void {
+        event.stopPropagation();
+        setClassNav('sm:w-4/12 absolute top-0 bottom-0 right-0 w-8/12 sm:relative sm:bg-transparent bg-components-dark z-10 hidden')
+    }
 
     return (
         <header className="flex flex-row items-center justify-between py-[25px] px-[160px]  border-b border-border-light dark:border-border-dark bg-back-light dark:bg-components-dark text-color-light dark:text-color-dark" >
             <h1>LOGO EMPRESA</h1>
-            <nav className="w-4/12">
-                <ul className="flex list-none flex-row items-center justify-between">
-                    <li><Link to="/"> Home</Link></li>
-                    <li> <Link to="/pages/sobre">Sobre</Link></li>
-                    <li><Link to="/pages/suport">Suport</Link></li>
-                    <li><Link to="/pages/login">Login</Link></li>
-                    <li><Link to="/pages/signIn">Sign In</Link></li>
+            <nav className={classNav}>
+                <ul className="flex list-none sm:flex-row sm:items-center sm:justify-between flex-col ">
+                    <span className='block h-8 py-[20px] px-[40px] sm:hidden'>Olá user</span>
+                    <li className={styleLiMobile}><Link className={styleLiLink} to="/"> Home</Link></li>
+                    <li className={styleLiMobile}> <Link className={styleLiLink} to="/pages/sobre">Sobre</Link></li>
+                    <li className={styleLiMobile}><Link className={styleLiLink} to="/pages/suport">Suport</Link></li>
+                    <li className={styleLiMobile}><Link className={styleLiLink} to="/pages/login">Login</Link></li>
+                    <li className={styleLiMobile}><Link className={styleLiLink} to="/pages/signIn">Sign In</Link></li>
+                    <li className='h-8 py-[10px] px-[40px] sm:hidden'><CircleX onClick={closeMenu} /></li>
+                    <li className='h-8 py-[10px] px-[40px] sm:hidden'><ThemeIcon  /></li>
                 </ul>
             </nav>
-            <ThemeIcon/>
+            <ThemeIcon />
+            <Menu className=" sm:hidden" onClick={openMenu} />
         </header>
     )
 }
