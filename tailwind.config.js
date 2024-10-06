70f20d6fd3d9fc61636f1e242a03adde7b62ad08
Jessica.Silva <jessica.silva@class-solutions.com.br>
2024-10-06 08:29:09 -0300

    Configurando arquivo para rodar cores tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
     extend: {
       colors: {
-        'bg-footer': '#001a4b', // Replace with your hex color
-        'bg-button-padrao': '#001a4b', // Replace with your hex color
+        "back-footer": "#001a4b", // Replace with your hex color
+        "back-button-padrao": "#001a4b", // Replace with your hex color
       },
     },
   },
